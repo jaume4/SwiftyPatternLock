22412c85616d0083969d20ae1ee717257bc37c00
Jaume <jaume4@gmail.com>
2019-08-28 21:51:11 +0200

    working drawing

--- Pattern/ViewController.swift
+++ Pattern/ViewController.swift
@@ -8,11 +8,170 @@
 
 import UIKit
 
+extension UIColor {
+
+    static func random(hue: CGFloat = CGFloat.random(in: 0...1),
+                       saturation: CGFloat = CGFloat.random(in: 0.5...1), // from 0.5 to 1.0 to stay away from white
+        brightness: CGFloat = CGFloat.random(in: 0.5...1), // from 0.5 to 1.0 to stay away from black
+        alpha: CGFloat = 1) -> UIColor {
+        return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: alpha)
+    }
+}
+
+extension UIView {
+    func pinEdges(to other: UIView) {
+        leadingAnchor.constraint(equalTo: other.leadingAnchor).isActive = true
+        trailingAnchor.constraint(equalTo: other.trailingAnchor).isActive = true
+        topAnchor.constraint(equalTo: other.topAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: other.bottomAnchor).isActive = true
+    }
+}
+
+extension CGPoint {
+    func distance(from point: CGPoint) -> CGFloat {
+        let dx = point.x - x
+        let dy = point.y - y
+        return sqrt((dx * dx) + (dy * dy))
+    }
+}
+
+extension CGRect {
+    var center: CGPoint {
+        return CGPoint(x: midX, y: midY)
+    }
+}
+
 class ViewController: UIViewController {
+
+    @IBOutlet weak var containerView: UIView!
+
+    var locationOfBeganTap: CGPoint!
+    var locationOfEndTap: CGPoint!
+    var recalculatedCenters = false
+
+    var parentStack: UIStackView!
+    var drawingLayer: CAShapeLayer!
+    var centers: [CGPoint]!
+    var minDistance: CGFloat!
+    var passedPoints: [Int]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+
+        var index = 0
+        var stacks = [UIStackView]()
+
+        for _ in 0..<3 {
+            let stack = UIStackView()
+            stack.axis = .horizontal
+            stack.translatesAutoresizingMaskIntoConstraints = false
+            stack.distribution = .fillEqually
+            stacks.append(stack)
+            for _ in 0..<3 {
+                let view = UIView()
+                view.accessibilityIdentifier = "\(index)"
+                index += 1
+                view.translatesAutoresizingMaskIntoConstraints = false
+                view.backgroundColor = UIColor.random()
+                stack.addArrangedSubview(view)
+            }
+        }
+        parentStack = UIStackView(arrangedSubviews: stacks)
+        parentStack.translatesAutoresizingMaskIntoConstraints = false
+        parentStack.axis = .vertical
+        parentStack.distribution = .fillEqually
+        containerView.addSubview(parentStack)
+        parentStack.pinEdges(to: containerView)
+
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(didMove(gesture:)))
+        containerView.addGestureRecognizer(panGesture)
+
+    }
+
+    func nearestPoint(from point: CGPoint) -> (index: Int, distance: CGFloat) {
+
+        let distances = centers.map{ $0.distance(from: point) }
+        let min = distances.min()!
+        return (distances.firstIndex(where: {$0 == min})!, min)
+
+    }
+
+    @objc func didMove(gesture: UIPanGestureRecognizer) {
+
+        switch gesture.state {
+        case .began:
+
+            calculatePointCenters()
+            locationOfBeganTap = gesture.location(in: parentStack)
+            let nearest = nearestPoint(from: locationOfBeganTap)
+            passedPoints = [nearest.index]
+            locationOfBeganTap = centers[nearest.index]
+//            let stack = minIndex / 3
+//            let index = minIndex % 3
+            locationOfEndTap = gesture.location(in: containerView)
+            draw(index: nearest.index, distance: nearest.distance)
+        case .changed:
+            locationOfEndTap = gesture.location(in: containerView)
+            let nearest = nearestPoint(from: locationOfEndTap)
+            draw(index: nearest.index, distance: nearest.distance)
+        case .ended:
+            locationOfEndTap = gesture.location(in: containerView)
+            let nearest = nearestPoint(from: locationOfEndTap)
+            draw(index: nearest.index, distance: nearest.distance, ending: true)
+        default: return
+        }
+
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        recalculatedCenters = false
+    }
+
+    func calculatePointCenters() {
+
+        if recalculatedCenters { return }
+        recalculatedCenters = true
+
+        centers = parentStack.arrangedSubviews.flatMap { stack in
+            (stack as! UIStackView).arrangedSubviews.compactMap{
+
+                if $0.accessibilityIdentifier == "0" { minDistance = $0.frame.width / 3 }
+                return $0.convert($0.bounds, to: containerView).center
+            }
+        }
+
+    }
+
+    func draw(index: Int, distance: CGFloat, ending: Bool = false) {
+
+        if drawingLayer == nil {
+            drawingLayer = CAShapeLayer()
+            containerView.layer.insertSublayer(drawingLayer, above: containerView.layer)
+        }
+
+        print(distance < minDistance && !passedPoints.contains(index))
+        print(index)
+
+        if distance < minDistance, !passedPoints.contains(index) {
+            passedPoints.append(index)
+        }
+
+        let path = UIBezierPath()
+        path.move(to: locationOfBeganTap)
+        passedPoints.forEach {
+            path.addLine(to: centers[$0])
+            path.move(to: centers[$0])
+        }
+        if !ending {path.addLine(to: locationOfEndTap)}
+
+
+        drawingLayer.path = path.cgPath
+        drawingLayer.strokeColor = UIColor.red.cgColor
+        drawingLayer.lineWidth = 2
+
     }
 
 
